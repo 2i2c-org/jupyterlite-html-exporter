031f089e330fdd33942ec8f1f14a50e655bb5613
Angus Hollands <goosey15@gmail.com>
2026-07-22 18:03:28 +0100

    fix: support running on JupyterLab

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,21 +1,25 @@
 import { ServiceManagerPlugin } from '@jupyterlab/services';
 import { INbConvertExporters } from '@jupyterlite/services';
-import { HTMLExporter } from './exporter';
 
 /**
  * Plugin to register custom exporters.
  */
-const exporterPlugin: ServiceManagerPlugin<void> = {
+export const exporterPlugin: ServiceManagerPlugin<void> = {
   id: 'jupyterlite-html-exporter:html-exporter',
   description: 'Exporter for Jupyter Notebooks to HTML.',
   autoStart: true,
-  requires: [INbConvertExporters],
-  activate: (_: null, exporters: INbConvertExporters): void => {
+  optional: [INbConvertExporters],
+  activate: async (
+    _: null,
+    exporters: INbConvertExporters | null
+  ): Promise<void> => {
+    if (exporters === null) {
+      return;
+    }
     // Register the custom exporter
+    const { HTMLExporter } = await import('./exporter');
     exporters.register('HTML', new HTMLExporter());
 
     console.log('Custom HTML exporter registered');
   }
 };
-
-export default exporterPlugin;
